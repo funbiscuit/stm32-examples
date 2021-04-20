5cf4672b7d964104f250b75adad96b14a2f4fa90
funbiscuit <kokurinsv@gmail.com>
2021-04-20 08:48:45 +0300

    Add basic blink example

---
+++ basic-blink/README.md
@@ -0,0 +1,4 @@
+# STM32H7 Example. Basic Blink.
+
+This example include simple blink code. LED, connected to PB7 will blink
+with 1Hz frequency.
---
+++ basic-blink/Src/user_run.c
@@ -0,0 +1,14 @@
+#include "user_run.h"
+
+#include <stdbool.h>
+
+#include "stm32h7xx_hal.h"
+
+_Noreturn void userRun(void)
+{
+    while (true)
+    {
+        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7);
+        HAL_Delay(500);
+    }
+}
